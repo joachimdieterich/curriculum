50798220be613c33578bf698280d90f7cc9712ab
joachimdieterich <>
2014-03-09 14:03:56 +0100

    Code enhancement

--- share/classes/exception.class.php
+++ share/classes/exception.class.php
@@ -24,6 +24,13 @@
  * http://www.gnu.org/copyleft/gpl.html      
  */
 class CurriculumException extends Exception  {
-    //not implemented 
+   
+    public function __construct($message, $code = 0) {
+        parent::__construct($message, $code);
+    }
+    
+    public function __toString() {
+        return __CLASS__ . ":[{$this->code}]: {$this->message}\n";
+    }
 }
 ?>
