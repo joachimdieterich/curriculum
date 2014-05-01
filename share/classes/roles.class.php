d2d451fdef23bef0bf8900f45332a3907646fd05
joachimdieterich <>
2014-05-01 10:42:05 +0200

    Resolve Issue #22

--- share/classes/roles.class.php
+++ share/classes/roles.class.php
@@ -129,14 +129,6 @@ class Roles {
     public function delete($creator_id = null){
         global $USER;
         if (checkCapabilities('role:delete', $USER->role_id)){
-           /* if ($creator_id != null) { // if function is called by request-php --> required by checkCapabilities()
-                $user = new USER();
-
-                $user->load('id', $creator_id);
-                $role_id = $user->role_id;
-            } else {
-                $role_id = $USER->role-id;
-            } */
             $db = DB::prepare('DELETE FROM user_roles WHERE role_id = ?');
             $delete_role =  $db->execute(array($this->role_id));
             $db = DB::prepare('DELETE FROM role_capabilities WHERE role_id= ?');
