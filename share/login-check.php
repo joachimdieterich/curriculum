ed9945c26d83fe68fa88d868e3b64ee6cff492cf
joachimdieterich <>
2014-06-13 09:22:45 +0200

    Fixes Issue #26

--- share/login-check.php
+++ share/login-check.php
@@ -27,7 +27,7 @@ if(isset($_SESSION['timein']) ) {        // check to see if $_SESSION['timein'] 
     $session_life = time() - $_SESSION['timein'];
     
     if($session_life > ($INSTITUTION->institution_timeout)*60){ //*60 to get minutes
-        if (method_exists($USER,'userLogout')){ //resolves Issue #26
+        if (method_exists($USER,'userLogout')){ //resolves Issue #26 
             $USER->userLogout();
         }
         session_destroy(); 
