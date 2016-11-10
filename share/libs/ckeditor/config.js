7dc2eee0556a4d8f7b8d56e5940745ef5573ac64
Joachim Dieterich <joachim.dieterich@gmx.net>
2016-11-10 19:47:09 +0100

    show font-awesome <i></i> tags in ckeditor

--- share/libs/ckeditor/config.js
+++ share/libs/ckeditor/config.js
@@ -4,31 +4,26 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-	config.skin = 'bootstrapck';
-	config.toolbar_Full = [
-    
-    // On the basic preset, clipboard and undo is handled by keyboard.
-    // Uncomment the following line to enable them on the toolbar as well.
+    config.skin = 'bootstrapck';        
+    config.toolbar_Full = [
     { name: 'clipboard',   groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', 'Undo', 'Redo' ] },
-    
-    { name: 'insert', items: [ 'CreatePlaceholder', 'Image', /*'Flash',*/ 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar'/*, 'PageBreak', 'Iframe', 'InsertPre'*/ ] },
-    /*{ name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },*/
+    { name: 'insert', items: [ 'CreatePlaceholder', 'Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar' ] },
     { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'RemoveFormat' ] },
-    { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ], items: [ 'NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote', /*'CreateDiv',*/ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'/*, 'BidiLtr', 'BidiRtl' */] },
-    { name: 'links', items: [ 'Link', 'Unlink'/*, 'Anchor'*/ ] },
-    /*'/',*/
+    { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ], items: [ 'NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+    { name: 'links', items: [ 'Link', 'Unlink' ] },
     { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
     { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
     { name: 'tools', items: [ 'UIColor', 'Maximize', 'ShowBlocks' ] },
-	{ name: 'editing',     groups: [ 'find', 'selection'/*, 'spellchecker'*/ ], items: [ 'Find', 'Replace', 'SelectAll'/*, 'Scayt' */] },
-	{ name: 'document',    groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', /*'Save', 'NewPage', 'DocProps',*/ 'Preview', 'Print', /*'Templates',*/ 'document' ] },
+    { name: 'editing',     groups: [ 'find', 'selection' ], items: [ 'Find', 'Replace', 'SelectAll' ] },
+    { name: 'document',    groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', 'Preview', 'Print', 'document' ] },
     { name: 'about', items: [ 'About' ] }
   ];
-  
-  config.protectedSource.push(/<i[^>]*><\/i>/g);   // ALLOW <i></i>
+ 
   config.toolbar = "Full";
   config.toolbarCanCollapse = true;
+  config.allowedContent = true;
+  CKEDITOR.dtd.$removeEmpty['i'] = false;
+  CKEDITOR.dtd.$removeEmpty.i = 0;
+  CKEDITOR.dtd.$removeEmpty['span'] = false;
+  config.contentsCss = ['../share/libs/font-awesome-4.6.1/css/font-awesome.min.css'];
 };
