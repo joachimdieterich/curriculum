5a61aa7260f3702e45b4427d92d106185cb1c0a1
Joachim Dieterich <joachim.dieterich@gmx.net>
2016-11-20 14:55:28 +0100

    shorter code

--- share/request/f_grade.php
+++ share/request/f_grade.php
@@ -69,15 +69,10 @@ $content .= Form::input_text('grade', 'Klassenstufe', $grade, $error, 'z. B. 7. 
 $content .= Form::input_text('description', 'Beschreibung', $description, $error, 'Beschreibung');
 $content .= Form::input_select('institution_id', 'Institution', $USER->institutions, 'institution', 'institution_id', $institution_id , $error);
 $content .= '</div></form>';
-$f_content = '';
-if ($func == 'edit'){ 
-    $f_content .= '<button type="submit" class="btn btn-primary fa fa-check-circle-o pull-right" onclick="document.getElementById(\'form_grade\').submit();"> '.$header.'</button>';
-} else {
-    $f_content .= '<button type="submit" class="btn btn-primary fa fa-plus pull-right" onclick="document.getElementById(\'form_grade\').submit();"> '.$header.'</button>';
-}
+$footer   = '<button type="submit" class="btn btn-primary pull-right" onclick="document.getElementById(\'form_grade\').submit();"><i class="fa fa-floppy-o margin-r-5"></i>'.$header.'</button>';
 
 $html     = Form::modal(array('title'     => $header,
                               'content'   => $content, 
-                              'f_content' => $f_content));  
+                              'f_content' => $footer));  
 
 echo json_encode(array('html'=>$html));
