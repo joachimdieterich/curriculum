d02e8f0d9ca93482bf1fc4fc9b60c7f2e3accda5
Joachim Dieterich <joachim.dieterich@gmx.net>
2016-12-28 06:15:36 +0100

    adding autoloader for classes

--- share/include.php
+++ share/include.php
@@ -24,62 +24,27 @@
 * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+include('classes/plugin.class.php');   // load plugin_base first to prevent errors in autoloader
+
 /* Klassen laden */
-include('classes/absent.class.php');
-include('classes/backup.class.php');
-include('classes/block.class.php');
-include('classes/capability.class.php');
-include('classes/certificate.class.php');
-include('classes/config.class.php');
-include('classes/content.class.php');
-include('classes/context.class.php');
-include('classes/country.class.php');
-include('classes/course.class.php'); 
-include('classes/coursebook.class.php'); 
-include('classes/cron.class.php');
-include('classes/curriculum.class.php'); 
-include('classes/db.class.php'); 
-include('classes/enablingobjective.class.php'); 
-include('classes/event.class.php'); 
-include('classes/exception.class.php'); 
-include('classes/file.class.php'); 
-
-include('classes/grade.class.php');     
-include('classes/group.class.php');  
-include('classes/gump.class.php');                  //validator class
-include('classes/help.class.php'); 
-include('classes/institution.class.php'); 
-include('classes/interval.class.php'); 
-include('classes/license.class.php'); 
-include('classes/log.class.php'); 
+/* Autoloader for classes */
+spl_autoload_register(function ($className) {
+    $base_dir = dirname(__FILE__). DIRECTORY_SEPARATOR;
+    $extensions = array(".php", ".class.php");
+    $paths = array('classes');
+    foreach ($paths as $path) {
+        $filename = $path . DIRECTORY_SEPARATOR . $className;
+        foreach ($extensions as $ext) {
+            if (is_readable($base_dir .$filename . $ext)) {
+                include ($base_dir . $filename . $ext);
+                break;
+           }
+       }
+    } 
+});
 global $LOG;
-$LOG = new Log();                                   // Funktion ist so  innerhalb des uploadframes sowie der einzelnen requests nutzbar
-include('classes/mail.class.php');
-include('classes/mailbox.class.php');               
-include('classes/node.class.php');               
-include('classes/pdf.class.php'); 
-include('classes/plugin.class.php');     
-include('classes/portfolio.class.php'); 
-include('classes/printer.class.php'); 
-include('classes/quiz_question.class.php'); 
-include('classes/quiz_answer.class.php');
-include('classes/roles.class.php');     
-include('classes/schooltype.class.php'); 
-include('classes/search.class.php'); 
-include('classes/semester.class.php'); 
-include('classes/state.class.php'); 
-include('classes/statistic.class.php'); 
-include('classes/subject.class.php');     
-include('classes/task.class.php');     
-include('classes/terminalobjective.class.php');
-include('classes/upload.class.php');  
-include('classes/user.class.php');      
+$LOG = new Log(); 
 
-/*Videostream*/
-include('classes/video_stream.class.php');     
-
-/*HTML Purifier*/
-require 'libs/htmlpurifier-4.7.0-standalone/HTMLPurifier.standalone.php';     
-
-
+require 'classes/video_stream.class.php';                                   //Videostream
+require 'libs/htmlpurifier-4.7.0-standalone/HTMLPurifier.standalone.php';   //HTML Purifier
 require 'libs/rtf-html-php-master/rtf-html-php.php';
