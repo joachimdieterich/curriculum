1fde840de0c1086710f43d799394430288706e66
Joachim Dieterich <joachim.dieterich@gmx.net>
2017-06-19 13:38:07 +0200

    remove status now is stored in db

--- share/request/f_block.php
+++ share/request/f_block.php
@@ -79,46 +79,75 @@ if (isset($_SESSION['FORM'])){
     }
 }
 
-$content  ='<form id="form_block"  class="form-horizontal" role="form" method="post" action="../share/processors/fp_block.php"';
+$content  ='<div class="nav-tabs-custom"> 
+              <ul class="nav nav-tabs">
+                <li class="active"><a href="#block_new" data-toggle="tab" aria-expanded="false">Neuer Block</a></li>
+                <li class=""><a href="#block_visible" data-toggle="tab" aria-expanded="true" >Ausgeblendete Blöcke</a></li>
+              </ul>
+              <div class="tab-content">
+                <div class="tab-pane active" id="block_new">
+                <form id="form_block"  class="form-horizontal" role="form" method="post" action="../share/processors/fp_block.php"';
 
-if (isset($currentUrlId)){ $content .= $currentUrlId; }
-$content .= '"><input type="hidden" name="func" id="func" value="'.$func.'"/>';
-if (isset($id)){
-    $content .= '<input type="hidden" name="id" id="id" value="'.$id.'"/>';
-}
-$content .= Form::input_select('block_id', 'Blocktyp', $types, 'block', 'id', $block_id , $error, 'formloader(\'block\',\'new\', this.value);');
+                if (isset($currentUrlId)){ $content .= $currentUrlId; }
+                $content .= '"><input type="hidden" name="func" id="func" value="'.$func.'"/>';
+                if (isset($id)){
+                    $content .= '<input type="hidden" name="id" id="id" value="'.$id.'"/>';
+                }
+                $content .= Form::input_select('block_id', 'Blocktyp', $types, 'block', 'id', $block_id , $error, 'formloader(\'block\',\'new\', this.value);');
 
-$content .= Form::input_text('name', 'Titel', $name, $error,'z.B. Links');
-/*get current type to render correct form elements*/
-foreach($types as $typ) {
-    if ($block_id == $typ->id) {
-        $t = $typ;
-        break;
-    }
-}
-if ($t->block == 'moodle'){
-    $content .= Form::input_text('moodle_block', 'Link zur Moodle Instanz', $configdata, $error,'http://example.de/moodle');
-}
-if ($t->block == 'html'){
-    $content .= Form::input_textarea('html_block', 'HTML-Code', $configdata, $error);
-}
-$c            = new stdClass();
-$c->id        = 11;
-$c->context   = 'Dashboard';
-$content     .= Form::input_select('context_id', 'Bereich', array($c), 'context', 'id', $context_id , $error);
-$r            = new stdClass();
-$r->id        = null;
-$r->region    = 'Übersicht';
-$content     .= Form::input_select('region', 'Position', array($r), 'region', 'id', $region , $error);
-// Sortierung neues input element generiern
+                $content .= Form::input_text('name', 'Titel', $name, $error,'z.B. Links');
+                /*get current type to render correct form elements*/
+                foreach($types as $typ) {
+                    if ($block_id == $typ->id) {
+                        $t = $typ;
+                        break;
+                    }
+                }
+                if ($t->block == 'moodle'){
+                    $content .= Form::input_text('moodle_block', 'Link zur Moodle Instanz', $configdata, $error,'http://example.de/moodle');
+                }
+                if ($t->block == 'html'){
+                    $content .= Form::input_textarea('html_block', 'HTML-Code', $configdata, $error);
+                }
+                $c            = new stdClass();
+                $c->id        = 11;
+                $c->context   = 'Dashboard';
+                $content     .= Form::input_select('context_id', 'Bereich', array($c), 'context', 'id', $context_id , $error);
+                $r            = new stdClass();
+                $r->id        = null;
+                $r->region    = 'Übersicht';
+                $content     .= Form::input_select('region', 'Position', array($r), 'region', 'id', $region , $error);
+                // Sortierung neues input element generiern
 
-$roles        = new Roles();
-$content     .= Form::input_select('role_id', 'Anzeigen für:', $roles->get(), 'role', 'id', $block_id , $error);
-$content     .= '</form>';
-$footer       = '<button type="submit" class="btn btn-primary pull-right" onclick="document.getElementById(\'form_block\').submit();"><i class="fa fa-floppy-o margin-r-5"></i>'.$header.'</button> ';
+                $roles        = new Roles();
+                $content     .= Form::input_select('role_id', 'Anzeigen für:', $roles->get(), 'role', 'id', $block_id , $error);
+                $content     .= '<button type="submit" class="btn btn-primary pull-right" onclick="document.getElementById(\'form_block\').submit();"><i class="fa fa-floppy-o margin-r-5"></i>'.$header.'</button><br><br>';
+                $content     .= '</form>  
+                 </div><!-- /.tab-pane -->
+                 
+                <div class="tab-pane " id="block_visible">';
+                $blocks                 = new Block();
+                $ct = new Context();
+                $ct->resolve('context', 'dashboard');
+                $blocks->context_id     = $ct->id;
+                $blocks->institution_id = $USER->institution_id;
+                $dashboar_blocks        = $blocks->get();
+                $removed_blocks       = array();
+                foreach ($dashboar_blocks as $key => $value) {
+                    if ($value->visible == 0){
+                        $removed_blocks[] = $value;
+                    }                    
+                }
+                $content     .= '<input type="hidden" name="set_visible_status" id="set_visible_status" value="true"/>';
+                $content     .= Form::input_select('block_id_visible', 'Block', $removed_blocks, 'name', 'id', '' , $error);
+                $content     .= '<button class="btn btn-primary pull-right" onclick="processor(\'config\',\'remove\', $( \'select#block_id_visible option:checked\' ).val());"><i class="fa fa-floppy-o margin-r-5"></i>Block einblenden</button><br><br></div><!-- /.tab-pane -->
+                                </div><!-- /.tab-content -->
+                              </div><!-- /.nav-tab-custom -->';
+
+
 
 $html         = Form::modal(array('title'     => $header,
                                   'content'   => $content, 
-                                  'f_content' => $footer));
+                                  'f_content' => ''));
 
 echo json_encode(array('html'=>$html));
