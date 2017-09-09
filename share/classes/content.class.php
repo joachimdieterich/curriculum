047d675cd168875e3abdc496c0f9a0c6d70fcae2
Joachim Dieterich <joachim.dieterich@gmx.net>
2017-09-09 10:06:24 +0200

    fixing bug "terms not shown"

--- share/classes/content.class.php
+++ share/classes/content.class.php
@@ -103,6 +103,12 @@ class Content {
                                                         AND cts.content_id = ct.id '.$order);
                             $db->execute(array($id));
                 break;
+            case 'terms':       $db = DB::prepare('SELECT ct.id FROM content AS ct, content_subscriptions AS cts, context AS co
+                                                        WHERE  co.context = "terms"
+                                                        AND co.context_id = cts.context_id
+                                                        AND cts.content_id = ct.id');
+                                $db->execute();
+                break;
 
             default:        
                             $db = DB::prepare('SELECT ct.id FROM content AS ct, content_subscriptions AS cts, context AS co
