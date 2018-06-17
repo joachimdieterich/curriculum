12a5cc2ec27e09fa1e9f024ff1a5bd21d5f26922
Joachim Dieterich <joachim.dieterich@gmx.net>
2018-06-17 11:48:35 +0200

    Update - adding new schooltype

---
+++ share/update/201806140901.php
@@ -0,0 +1,44 @@
+<?php
+/** Updatefile 12 - Adding new subject
+* 
+* @abstract This file is part of curriculum - http://www.joachimdieterich.de
+* @package core
+* @filename 201806171146.php
+* @copyright 2018 Joachim Dieterich
+* @author Joachim Dieterich
+* @date 2018.06.17 11:46:00
+* @license: 
+*
+* The MIT License (MIT)
+* Copyright (c) 2012 Joachim Dieterich http://www.curriculumonline.de
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
+* THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+global $UPDATE;
+$UPDATE         = new stdClass();
+$UPDATE->info   = "Adding new Subject. <br><br> Fügt neues Fach Darstellendes Spiel hinzu.";
+
+if (isset($_GET['execute'])){
+    $UPDATE->log = "Starte Update... <br>";
+    $db= DB::prepare("INSERT INTO `subjects` (`subject`, `description`, `creation_time`, `creator_id`, `institution_id`, `subject_short`)
+VALUES
+	('Darstellendes Spiel', 'Darstellendes Spiel', '2018-06-17 11:45:45', 102, 0, 'DS');
+
+");
+    if ($db->execute(array())){
+        $UPDATE->log .= "<b class=\"text-success\">Update finished - OK</b><br>";
+        $UPDATE->installed = true;
+    } else {
+        $UPDATE->log .= "<b class=\"text-red\">Update finished - failed.</b><br>";
+        $UPDATE->installed = false;
+    }      
+}
