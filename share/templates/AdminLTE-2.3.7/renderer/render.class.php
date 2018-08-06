b5ac301e472f2a7f7f78972cd6ea0593bd08ca3f
Joachim Dieterich <joachim.dieterich@gmx.net>
2018-08-06 10:03:22 +0200

    Enhance Navigator

--- share/templates/AdminLTE-2.3.7/renderer/render.class.php
+++ share/templates/AdminLTE-2.3.7/renderer/render.class.php
@@ -1916,7 +1916,11 @@ class Render {
                             $enroled_groups     = $cur->getGroupsByUserAndCurriculum($USER->id);
                             $file_id            = $cur->icon_id;
                             $widget_onclick     = "location.href='index.php?action=view&curriculum_id={$nb_reference_id}&group={$enroled_groups[0]->group_id}';";
+                            /*if (checkCapabilities('navigator:add', $USER->role_id, false)){
                             $opt[]              = '<a type="button" style="color:#FFF;" class="fa fa-edit" onclick="formloader(\'navigator_item\', \'edit\','.$nb_id.', {\'nb_navigator_view_id\':\''.$nb_navigator_view_id.'\'})";></a>';
+                            } else {
+                               $opt[]              = null; 
+                            }*/
                             $html               = RENDER::paginator_widget(array('widget_title' => $nb_title, 'widget_desc' => $nb_description, 'file_id' => $file_id, 'widget_onclick' => $widget_onclick, 'opt' => $opt, 'global_onclick' => true));
                     break;
                 case 15:    $content            = new Content();
