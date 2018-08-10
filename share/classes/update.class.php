5514946c4b9783cbbbc51c1b0be4a27452a3aa5d
Joachim Dieterich <joachim.dieterich@gmx.net>
2018-08-10 10:19:31 +0200

    fix #138 update.php generates empty db entry

--- share/classes/update.class.php
+++ share/classes/update.class.php
@@ -24,7 +24,7 @@
 * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
 * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-class Update {
+class Updates {
     public $id;
     public $filename;
     public $description;
