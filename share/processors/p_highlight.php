82db77a5c24d90534acf71376f5f605049c4f7f7
joachimdieterich <>
2018-08-22 08:12:45 +0200

    fix enhance search function on view.tpl #144

---
+++ share/processors/p_highlight.php
@@ -0,0 +1,47 @@
+<?php
+/** This file is part of curriculum - http://www.joachimdieterich.de
+* 
+* @package core
+* @filename p_highlight.php
+* @copyright 2018 Joachim Dieterich
+* @author Joachim Dieterich
+* @date 2018.08.20 11:29
+* @license: 
+*
+* The MIT License (MIT)
+* Copyright (c) 2012 Joachim Dieterich http://www.curriculumonline.de
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
+* THE USE OR OTHER DEALINGS IN THE SOFTWARE.     
+*/
+include(dirname(__FILE__).'/../setup.php');  // Klassen, DB Zugriff und Funktionen
+include(dirname(__FILE__).'/../login-check.php');  //check login status and reset idletimer
+global $USER;
+$USER       = $_SESSION['USER'];
+$id         = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+$search     = filter_input(INPUT_GET, 'search', FILTER_UNSAFE_RAW);
+
+$s          = new Search();
+$s->id      = $id;
+$s->search  = $search;
+$result     = $s->view();   
+
+$highlight  = array();
+foreach($result AS $r){
+    if (isset($r->enabling_objective)){
+        $highlight[] = 'ena_'.$r->id;
+    } else {
+        $highlight[] = 'ter_'.$r->id;
+    } 
+}
+if (!empty($highlight)){
+    echo json_encode($highlight);   //return json encoded array of matches
+} 
