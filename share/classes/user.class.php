88a93a83388de8a3a386500d4627834d433024c5
joachimdieterich <joachimdieterich@mz-suw-ld-mbp.fritz.box>
2018-09-07 08:53:40 +0200

    fixing: Existing users are not enrolled to groups on import #150

--- share/classes/user.class.php
+++ share/classes/user.class.php
@@ -330,10 +330,14 @@ class User {
         $db = DB::prepare('SELECT COUNT(id) FROM users WHERE UPPER(username) = UPPER(?)');
         $db->execute(array($this->username));
         if($db->fetchColumn() >= 1) { 
+            $db1 = DB::prepare('SELECT id FROM users WHERE  UPPER(username) = UPPER(?)'); //load user id to get group enrolment working issue #150
+            $db1->execute(array($this->username));
+            $result = $db1->fetchObject(); 
+            $this->id = $result->id;
             $PAGE->message[] = array('message' => 'Benutzer '.$this->firstname.' '.$this->lastname.'('.$this->username.') existiert bereits', 'icon' => 'fa fa-user text-warning');// Schließen und speichern
             if (is_array($institution_id)){                                          //duplicate (s. else statement)-> make function 
                     $this->enroleToInstitutions($institution_id);                    //enrol to multiple Institutions
-                } else {
+                } else {  
                     $this->enroleToInstitution($institution_id);                    // enrol to one Institution
                     if (is_int($group_id)){                                         // enrol to group if id is set
                         $db_01 = DB::prepare('SELECT COUNT(id) FROM groups WHERE id = ? AND institution_id = ?'); //check if group is enroled to given institution
@@ -661,6 +665,7 @@ class User {
             } else { 
                 $db                 = DB::prepare('INSERT INTO groups_enrolments (status,group_id,user_id,creator_id) VALUES (1,?,?,?)');  //Status 1 == enroled
                 if ($db->execute(array($group_id, $this->id, $USER->id))){
+                    
                     $_SESSION['PAGE']->message[]    = array('message' => '<strong>'.$this->username.'</strong> in <strong>'.$groups->group.'</strong> eingeschrieben.', 'icon' => 'fa-user text-success');
                 }
             }   
